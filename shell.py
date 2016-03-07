e8fdc7e5d8d647c09c9922bc2de9a50c4ca84f1d
timfeirg <kkcocogogo@gmail.com>
2016-03-07 14:14:44 +0800

    extend shell.py to support ipython notebook

--- shell.py
+++ shell.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import atexit
 import os
 import sys
-import atexit
-import IPython
+
+import six
 
 from eru.app import create_app_with_celery
+
 
 def hook_readline_hist():
     try:
@@ -30,8 +31,6 @@ def hook_readline_hist():
             print 'Unable to save Python command history'
     atexit.register(savehist)
 
-def get_banner():
-    return 'ERU shell.'
 
 def pre_imports():
     from eru.models.base import Base
@@ -47,12 +46,8 @@ def pre_imports():
     return locals()
 
 def ipython_shell(user_ns):
-    if getattr(IPython, 'version_info', None) and IPython.version_info[0] >= 1:
-        from IPython.terminal.ipapp import TerminalIPythonApp
-        from IPython.terminal.interactiveshell import TerminalInteractiveShell
-    else:
-        from IPython.frontend.terminal.ipapp import TerminalIPythonApp
-        from IPython.frontend.terminal.interactiveshell import TerminalInteractiveShell
+    from IPython.terminal.ipapp import TerminalIPythonApp
+    from IPython.terminal.interactiveshell import TerminalInteractiveShell
 
     class ShireIPythonApp(TerminalIPythonApp):
         def init_shell(self):
@@ -61,7 +56,7 @@ def ipython_shell(user_ns):
                 display_banner=False,
                 profile_dir=self.profile_dir,
                 ipython_dir=self.ipython_dir,
-                banner1=get_banner(),
+                banner1=lambda: 'ERU shell.',
                 banner2=''
             )
             self.shell.configurables.append(self)
@@ -74,9 +69,64 @@ def ipython_shell(user_ns):
     with eru_app.app_context():
         sys.exit(app.start())
 
+def get_notebook():
+    from notebook.notebookapp import NotebookApp
+
+    def install_kernel_spec(app, display_name, ipython_arguments):
+        ksm = app.kernel_spec_manager
+        try_spec_names = ['python2', 'python']
+        if isinstance(try_spec_names, six.string_types):
+            try_spec_names = [try_spec_names]
+        ks = None
+        for spec_name in try_spec_names:
+            try:
+                ks = ksm.get_kernel_spec(spec_name)
+                break
+            except:
+                continue
+        if not ks:
+            raise Exception("No notebook (Python) kernel specs found")
+        ks.argv.extend(ipython_arguments)
+        ks.display_name = display_name
+
+        current_dir, this_script = os.path.split(os.path.realpath(sys.argv[0]))
+
+        if this_script == 'shell.py' and os.path.isdir(current_dir) and current_dir != os.getcwd():
+            pythonpath = ks.env.get('PYTHONPATH', os.environ.get('PYTHONPATH', ''))
+            pythonpath_list = pythonpath.split(':')
+            if current_dir not in pythonpath_list:
+                pythonpath_list.append(current_dir)
+
+            ks.env['PYTHONPATH'] = ':'.join(filter(None, pythonpath_list))
+
+        kernel_dir = os.path.join(ksm.user_kernel_dir, 'eru_kernel')
+        if not os.path.exists(kernel_dir):
+            os.makedirs(kernel_dir)
+        with open(os.path.join(kernel_dir, 'kernel.json'), 'w') as f:
+            f.write(ks.to_json())
+
+    def run_notebook():
+        app = NotebookApp.instance()
+
+        # Treat IPYTHON_ARGUMENTS from settings
+        ipython_arguments = []
+        notebook_arguments = ['--no-browser']
+
+        app.initialize(notebook_arguments)
+
+        display_name = 'eru-shell'
+        install_kernel_spec(app, display_name, ipython_arguments)
+
+        app.start()
+
+    return run_notebook
+
+
 def main():
-    hook_readline_hist()
-    ipython_shell(pre_imports())
+    # hook_readline_hist()
+    # ipython_shell(pre_imports())
+    get_notebook()()
+
 
 if __name__ == '__main__':
     main()
